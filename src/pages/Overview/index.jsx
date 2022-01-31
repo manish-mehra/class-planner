e6a4dbed83c5ebca22b8a4c6607a1a23d05d0ccb
manish-mehra <manishmehra1469@gmail.com>
2022-01-31 14:11:03 +0530

    setup tailwind css

--- src/pages/Overview/index.jsx
+++ src/pages/Overview/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function index() {
   return (
     <div>
-        <h1>Overview</h1>
+        <h1 className="text-3xl font-bold underline">Overview</h1>
     </div>
   )
 }
