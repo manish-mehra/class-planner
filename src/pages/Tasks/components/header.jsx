dfc8b33434e315e14be6f4f9e2658feb9189ca70
manish-mehra <manishmehra1469@gmail.com>
2022-02-03 21:09:52 +0530

    style: addtask card

--- src/pages/Tasks/components/header.jsx
+++ src/pages/Tasks/components/header.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 import {BsSearch} from 'react-icons/bs'
 import {FiFilter} from 'react-icons/fi'
 import {GrAdd} from 'react-icons/gr'
 
+//import components
+import AddTask from './AddTask'
+
 export default function Header() {
+
+    const [addTask, setAddTask] = useState(false)
+
   return (
       <div className='flex-col mt-14'>
+          {/* Add task component */}
+        {
+            addTask? 
+                <div>
+                    <AddTask showModal = {setAddTask}/>
+                </div>
+            : null
+        }
           {/* top section */}
         <div className='flex justify-between items-center'>
             <h1 className='text-lg font-semibold text-gray-700'>Tasks</h1>
             
             <div className='flex items-center  justify-between'>
                 
-                <div className='mr-6 p-1 hover:bg-green-200 rounded-sm'>
-                    <GrAdd/>
+                <div 
+                className='mr-6 p-1 hover:bg-green-200 rounded-sm'
+                onClick={()=>setAddTask((prev)=> !prev)}
+                >
+                    <p>
+                        <GrAdd/>
+                    </p>
                 </div>
 
                 <span className='text-gray-600 flex items-center py-1'>
