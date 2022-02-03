b65cc9c7bbc6f8b5a3dbd7232bbd377190e965b7
manish-mehra <manishmehra1469@gmail.com>
2022-02-03 18:19:18 +0530

    style: task card

--- src/pages/Tasks/components/TaskCard.jsx
+++ src/pages/Tasks/components/TaskCard.jsx
@@ -1,32 +1,45 @@
 import React from 'react'
 import {BsThreeDotsVertical} from 'react-icons/bs'
 import {BsFlag} from 'react-icons/bs'
+import {AiOutlineSchedule} from 'react-icons/ai'
+import {MdOutlineLabel} from 'react-icons/md'
+
 
 export default function TaskCard() {
   return(
-    <div className='flex justify-between mt-2 pb-4 border border-transparent border-b-gray-300'>
-        
-        <div className='flex-col'>
+    <div className='flex flex-col mt-3 border border-gray-200 p-3 rounded-md shadow-sm'>
+          <section className='flex flex-col mb-5'>
+                <div className='flex justify-between'>
+                    <span className='outline-none mb-1 text-sm font-semibold'>
+                        Finish task management
+                    </span>
+                    <div className='flex gap-1'>
+                        <BsFlag/>
+                        <BsThreeDotsVertical/>
+                    </div>
+                </div>
+                <span 
+                    placeholder="Description"
+                    className='outline-none text-xs'
+                >
+                    I should absolutely finish this
+                </span>
+          </section>
 
-            <div className='flex items-center'>
-                <input type="checkbox" name = "todo"/>
-                <h1 className='text-base text-gray-700 ml-2'>This is the title of a todo list</h1>
-            </div>
-            <div>
-                <p className='text-sm text-green-800'>24-8-2022</p>
-                <p className='text-gray-600'>this is a description which is very long</p>
-            </div>
-        </div>
+          <section className='flex justify-between mt-3 items-center'>
+                <div className='hover:bg-green-200 p-1 rounded-md border border-gray-300 cursor-pointer'>
+                    <span className='flex items-center'>
+                        <p className='text-xs font-semibold'>Tomorrow</p>
+                    </span>
+                </div>
 
-        <div className='flex gap-2'>
-            <p>
-                <BsFlag/>
-            </p>
-            <p>
-                <BsThreeDotsVertical/>
-            </p>
-        </div>
-
-    </div>
+                <div className='flex gap-2'>
+                    <span className='flex gap-1 text-xs'>
+                        <p className='bg-green-200 p-1 rounded-sm cursor-pointer'>english</p>
+                        <p className='bg-green-200 p-1 rounded-sm cursor-pointer'>math</p>
+                    </span>
+                </div>
+          </section>
+      </div>
   )
 }
